4ad5ec582a5f11e216448d3a86e8535d5b76272c
A1daxiang <a1daxiang@163.com>
2020-10-09 15:53:05 +0800

    收尾导航

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- <FooterNav></FooterNav> 简写成如下 -->
+    <FooterNav/>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+// 导入需要使用的组件
+import FooterNav from '@/components/FooterNav'
+export default {
+  components:{
+    FooterNav,
+  },
 }
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<style lang="scss" >  //scoped 作用范围 只在当前组件生效 所以不能写
+    *{
+        margin: 0;
+        padding: 0;
     }
-  }
-}
+    html,
+    body{
+        height: 100%;
+        touch-action: none; //禁止缩放
+    }
+    ul,li{
+        list-style: none;
+    }
 </style>
+
+
